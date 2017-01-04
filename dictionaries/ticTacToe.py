103b8a3b802c700a0e773f8c82d5478c6388e724
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-04 19:12:39 +0000

    Added fn to display board

--- dictionaries/ticTacToe.py
+++ dictionaries/ticTacToe.py
@@ -3,7 +3,12 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
 
-
+def printBoard(board):
+	print(board['top-L'] + | + print(board['top-M'] + | + print(board['top-R'] 
+	print('-+-+-')
+	print(board['mid-L'] + | + print(board['mid-L'] + | + print(board['mid-L'] 
+	print('-+-+-')
+	print(board['low-L'] + | + print(board['low-L'] + | + print(board['low-L']   
 
 
 print(theBoard)
