f25188c2c86daeb26002232c38a01ce39f4e2cb1
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-04 17:08:53 +0000

    Solved result, according to question, looped through x coordinates on inner loop, and y cooridinates on outer loop

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -11,16 +11,13 @@ grid = [
 
 
 		
-for i in range(len(grid)):
-	for j in range(len(grid[i])):
-		print(grid[i][j],end= ' ')
-	print()
+for j in range(len(grid[0])):
+	for i in range(len(grid)):
+		print(grid[i][j],end = ' ')
+	print()	
+	
+		
+
 			
 
-#print(grid[0])	 
-# print(grid[0][1])
-#print(grid[0][2])
-#print(grid[0][3])
-#print(grid[0][4])
-#print(grid[0][5])
 
