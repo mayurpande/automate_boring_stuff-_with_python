7bcce40dc12f9fb2e1892ab4d024d4e0d97ae195
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-05 22:41:02 +0000

    completed first and third steps, used pyperclip.paste to paste text, and pyperclip.copy() to copy text respectively

--- strings/bulletPointAddery.py
+++ strings/bulletPointAddery.py
@@ -1,1 +1,14 @@
 #! /usr/bin/python3
+
+# bullerPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard
+
+import pyperclip
+
+#paste text from clipboard
+text = pyperclip.paste()
+
+# TODO:Separate lines and add stars
+
+#copy the new text to the clipboard
+pyperclip.copy(text) 
