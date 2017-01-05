f2a2391a6b584df0b3add527cf72769b9fe928cd
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-05 20:39:18 +0000

    created password manager file, used shebang at top of file to run python scripts

---
+++ strings/pw.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+# pw.py - A password manager program
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
