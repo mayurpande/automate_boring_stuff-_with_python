405643c5ca7b6702093658b3ffb4e09098b676ab
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-05 11:46:41 +0000

    Attempting to make tic-tac-toe game

--- dictionaries/ticTacToe.py
+++ dictionaries/ticTacToe.py
@@ -6,10 +6,27 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
 def printBoard(board):
 	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R']) 
 	print('-+-+-')
-	print(board['mid-L'] +  '|' + board['mid-L'] + '|' + board['mid-L'])
+	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
 	print('-+-+-')
-	print(board['low-L'] + '|' + board['low-L'] + '|' + board['low-L'])   
+	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])   
 
 
-printBoard(theBoard)
+	
 
+def playerTurn(turn):
+	if turn == 'x':
+		turn == 'o'
+	else:
+		 turn == 'x'
+		
+
+def makeMove():
+	for i in range(9):
+		print('Enter move for player ' + turn + ' for position on board')
+		move = input()
+		theBoard[move] = turn
+		playerTurn(turn)
+		printBoard(theBoard)
+
+
+makeMove()
