1a30f6bc3f97b8a53039c7d21510d7be51bac50f
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-05 22:53:16 +0000

    joined list items back together with join command

--- strings/bulletPointAddery.py
+++ strings/bulletPointAddery.py
@@ -11,7 +11,7 @@ text = pyperclip.paste()
 # Separate lines and add stars
 lines = text.split('\n')
 for i in range(len(lines)):	#loop through all indexes in the "lines" list
-	line[i] = '* ' + lines[i] #add star to each string in "lines" list
-
+	lines[i] = '* ' + lines[i] #add star to each string in "lines" list
+text = '\n'.join(lines)
 #copy the new text to the clipboard
 pyperclip.copy(text) 
