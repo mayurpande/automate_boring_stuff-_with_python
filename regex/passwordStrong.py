fddb32d04e9848307e2f508b60d7219e7b69428e
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-07 16:32:37 +0000

    Tried to use carrot and dollar sign for regex object but doesn't work

--- regex/passwordStrong.py
+++ regex/passwordStrong.py
@@ -11,9 +11,9 @@ import re
 while True:
 	print('Please enter pw, must be 8 characters long and contain one digit, one uppercase letter, and one lowercase letter')
 	pw = input()
-	pwChecker = re.compile(r'[a-zA-Z0-9]{8,}')
-	mo = pwChecker.search(pw)
-	if mo == None:
+	pwChecker = re.compile(r'^\w\d$')
+	mo = pwChecker.findall(pw)
+	if len(mo) == 0:
 		continue
 	else:
 		break
