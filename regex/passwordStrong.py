e9df92d3fcfb133c7a246df7624ba8ffc3c8394a
Mayur Pande <mayurpande.uk@gmail.com>
2017-01-07 00:15:38 +0000

    Building while loop with regex expression for strong password

---
+++ regex/passwordStrong.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+# passwordStrong.py - a program to make user input enter a strong password
+# using regex, this must be 8 characters long, have at least on uppercase and
+# one lower case and one digit
+
+
+import re
+
+
+while True:
+	print('Please enter pw, must be 8 characters long and contain one digit, one uppercase letter, and one lowercase letter')
+	pw = input()
+	pwChecker = re.compile(r'[a-zA-Z0-9]{8,}')
+	mo = pwChecker.search(pw)
+	if mo.group() == None:
+		continue
+	else:
+		break
+
+print('Welcome you have securely logged in!')
+
